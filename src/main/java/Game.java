1c1a132bfb18b2355ba01444a237962a442f11e8
Chris Coulbeck <cdcoulbeck@gmail.com>
2025-06-24 16:01:46 +1200

    Integrate Deck into Game for card initialization and shuffling

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,3 +1,29 @@
+import java.util.*;
+
 public class Game {
-    public void play() {}
+    private final Deck deck;
+
+    public Game() {
+        List<Card> cards = createStandardCards();
+        this.deck = new Deck(cards);
+    }
+
+    public void play() {
+        this.deck.shuffle();
+    }
+
+    /**
+     * Creates a list of standard playing cards with all combinations of {@link Rank} and {@link Suit}.
+     *
+     * @return a list of {@code Card} objects
+     */
+    private List<Card> createStandardCards() {
+        List<Card> cards = new ArrayList<>();
+        for (Suit suit : Suit.values()) {
+            for (Rank rank : Rank.values()) {
+                cards.add(new Card(rank, suit));
+            }
+        }
+        return cards;
+    }
 }
